6755262f1de8553456898f544cafa7cb2abb2b95
João Lucas <ps.joaolucas@gmail.com>
2024-08-12 13:20:43 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -25,6 +28,30 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+        <h2 className="mb-6 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="http://localhost:3000/_next/image?url=%2Fbanner-01.png&w=640&q=75"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia do Jão</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">12</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
