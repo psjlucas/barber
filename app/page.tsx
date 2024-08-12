c08d1cfb36520ec740d842d817ed99ce8d050db3
João Lucas <ps.joaolucas@gmail.com>
2024-08-12 13:01:27 -0300

    feat: add banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -15,6 +16,14 @@ export default function Home() {
           <Button>
             <SearchIcon />
           </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
         </div>
       </div>
     </div>
