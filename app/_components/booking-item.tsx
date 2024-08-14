0f8e31f6cac47c661dc561cf6462983f09857e3b
João Lucas <ps.joaolucas@gmail.com>
2024-08-14 13:31:19 -0300

    feat: add BookingItem component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,36 @@
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
+
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mb-6 mt-6 text-xs font-bold uppercase text-gray-400">
+        Agendamentos
+      </h2>
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          {/* ESQUERDA */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="http://localhost:3000/_next/image?url=%2Fbanner-01.png&w=640&q=75"></AvatarImage>
+              </Avatar>
+              <p className="text-sm">Barbearia do Jão</p>
+            </div>
+          </div>
+          {/* DIREITA */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">12</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react"
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
@@ -52,30 +51,8 @@ export default async function Home() {
             className="rounded-xl object-cover"
           />
         </div>
-        <h2 className="mb-6 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="http://localhost:3000/_next/image?url=%2Fbanner-01.png&w=640&q=75"></AvatarImage>
-                </Avatar>
-                <p className="text-sm">Barbearia do Jão</p>
-              </div>
-            </div>
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">12</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+
+        <BookingItem />
 
         <h2 className="mb-6 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
