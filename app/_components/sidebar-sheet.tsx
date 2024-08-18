ec82e17cfca5558bbccf6f03fd291333bd4e9810
João Lucas <ps.joaolucas@gmail.com>
2024-08-18 20:35:41 -0300

    feat: add sidebar sheet

---
+++ app/_components/sidebar-sheet.tsx
@@ -0,0 +1,68 @@
+import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
+import Image from "next/image"
+
+const SidebarSheet = () => {
+  return (
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+
+      <div className="flex items-center gap-3 border-b border-solid py-5">
+        <Avatar>
+          <AvatarImage src="http://localhost:3000/_next/image?url=%2Fbanner-01.png&w=640&q=75"></AvatarImage>
+        </Avatar>
+        <div>
+          <p className="font-bold">João Lucas Pereira</p>
+          <p className="text-xs">ps.joaolucas@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamentos
+        </Button>
+      </div>
+      <div className="flex flex-col gap-1 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <Button
+            className="justify-start gap-2"
+            variant="ghost"
+            key={option.title}
+          >
+            <Image
+              alt={option.title}
+              src={option.imageUrl}
+              height={18}
+              width={18}
+            />
+            {option.title}
+          </Button>
+        ))}
+      </div>
+      <div className="flex flex-col gap-2 py-5">
+        <Button variant="ghost" className="justify-start gap-2">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
+  )
+}
+
+export default SidebarSheet
